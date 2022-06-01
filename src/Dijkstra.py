bef9d750dc84ec345db503390c50f99a314c2589
HanselTanoto <hansel.tanoto05@gmail.com>
2022-06-01 15:25:56 +0700

    Add GUI

--- src/Dijkstra.py
+++ src/Dijkstra.py
@@ -17,11 +17,14 @@ class Dijkstra:
         self.unvisited.append(start)                                # Start node is added to unvisited list
         self.path = []                                              # Path from start to end node
         self.distance = 0                                           # Distance from start to end node
+        self.iterate = 0                                            # Iteration count
+        self.step = []                                              # Step Information
         self.solve()                                                # Find the shortest path from start to end node
         
     # Dijkstra's Algorithm
     def solve(self):
         while self.unvisited:
+            self.iterate += 1
             u = self.unvisited.pop(0)
             self.visited[u] = True
             for v in self.graph.get_neighbors(u):
@@ -31,6 +34,7 @@ class Dijkstra:
                     self.shortest_dist[v] = self.shortest_dist[u] + self.graph.get_weight(u, v)
                     self.prev_node[v] = u
                     self.unvisited.append(v)
+            self.step.append(self.shortest_dist[:])
         self.path = self.get_path()
         self.distance = self.shortest_dist[self.end]
     
@@ -41,8 +45,28 @@ class Dijkstra:
             path.append(self.prev_node[path[-1]])
         return path[::-1]
     
+    # Get path from start to end node as a string
+    def get_path_str(self):
+        result = ""
+        for i in range(len(self.path)):
+            if (i == 0):
+                result += str(self.path[i])
+            else:
+                result += " ==> " + str(self.path[i])
+        return result
+    
     # Print path from start to end node
     def print_path(self):
         print("Shortest path from", self.start, "to", self.end, "is:", end=" ")
         print(self.path)
         print("Distance:", self.distance)
+    
+    # Print step information
+    def get_step_info(self, i):
+        result = ""
+        for j in range(self.graph.nodes):
+            if (self.step[i][j] == INFTY):
+                result += "   Node" + str(j) + "   :   " + "-" + "\n"
+            else:
+                result += "   Node" + str(j) + "   :   " + str(self.step[i][j]) + "\n"
+        return result
